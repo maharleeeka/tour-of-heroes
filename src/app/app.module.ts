7c454bf4da6cd89ad6488db15318206bfc20c8cf
Maharlika Alberca <maharlikaalberca@gmail.com>
2017-10-18 14:59:13 +0800

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { AppComponent } from './app.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
-
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
@@ -19,10 +20,19 @@ import { HeroService }         from './hero.service';
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
+        {
+            path: 'heroes',
+            component: HeroesComponent
+        },
+        {
+            path: 'dashboard',
+            component: DashboardComponent
+        },
+        {
+            path: '',
+            redirectTo: '/dashboard',
+            pathMatch: 'full'
+        },
     ])
   ],
   providers: [
